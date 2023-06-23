cf462552a29877c1644cae6dac7a114f505b8de8
Jude Shorbaji <jshorbaji@hillers.org>
2023-06-23 16:15:08 -0400

    Initial Commit

---
+++ about me fr/about_me_frApp.swift
@@ -0,0 +1,17 @@
+//
+//  about_me_frApp.swift
+//  about me fr
+//
+//  Created by Jude Shorbaji on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct about_me_frApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
